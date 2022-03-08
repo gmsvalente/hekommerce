a8ca5095c07f17923dab635c363b80e6eeb54b1b
gmsvalente <gustavomsvalente@gmail.com>
2022-03-07 23:40:02 -0300

    Add heroku postgresql env

--- src/hekommerce/backend/db.clj
+++ src/hekommerce/backend/db.clj
@@ -1,16 +1,17 @@
 (ns hekommerce.backend.db
-  (:require [xtdb.api :as xt]))
+  (:require [xtdb.api :as xt]
+            [environ.core :refer [env]]))
+
+(def database-url
+  (or (:jdbc-database-url env)
+      "jdbc:postgresql://localhost:5432/userdb?password=password&user=user"))
 
 (def node (xt/start-node
-           {:xtdb.jdbc/connection-pool
-            {:dialect {:xtdb/module 'xtdb.jdbc.psql/->dialect}
-             :db-spec {:host "localhost"
-                       :port "5432"
-                       :dbname "userdb"
-                       :user "user"
-                       :password "password"}}
-            :xtdb/tx-log {:xtdb/module 'xtdb.jdbc/->tx-log
-                          :connection-pool :xtdb.jdbc/connection-pool}
-            :xtdb/document-store {:xtdb/module 'xtdb.jdbc/->document-store
-                                  :connection-pool :xtdb.jdbc/connection-pool}}))
+               {:xtdb.jdbc/connection-pool
+                {:dialect {:xtdb/module 'xtdb.jdbc.psql/->dialect}
+                 :db-spec {:jdbcUrl database-url}}
+                :xtdb/tx-log {:xtdb/module 'xtdb.jdbc/->tx-log
+                              :connection-pool :xtdb.jdbc/connection-pool}
+                :xtdb/document-store {:xtdb/module 'xtdb.jdbc/->document-store
+                                      :connection-pool :xtdb.jdbc/connection-pool}}))
 
